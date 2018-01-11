9bbe7d68cf1477567734e7af872c27eee5dffb38
Joseph Surin <joseph.surin@gmail.com>
2018-01-11 16:17:52 +0800

    problem 48

---
+++ problem 48/problem48.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+
+public class problem48 {
+	public static void main(String[] args) {
+		BigInteger sum = BigInteger.ZERO;
+		for (int i = 1; i <= 1000; i++) {
+			sum = sum.add(BigInteger.valueOf(i).pow(i));
+		}
+		System.out.println(sum.toString().substring(sum.toString().length()-10));
+	}
+}
